328c9f58ed0be1f5f20c9c8a0f55cc38db523b2f
Ananatzu1 <118306604+Ananatzu1@users.noreply.github.com>
2024-05-31 11:43:50 +0200

    Uploaded Mem_dump

--- RISC-V/Load-Store/VHD-Files/Output_functions_package.vhd
+++ RISC-V/Load-Store/VHD-Files/Output_functions_package.vhd
@@ -1,16 +1,12 @@
 ----------------------------------------------------------------------------------
 -- Company: 
--- Engineer: 
+-- Engineer: Tiemo SCHMIDT
 -- 
 -- Create Date: 24.05.2024 14:27:22
--- Design Name: Tiemo Schmidt
+-- Design Name:
 -- Module Name: Output_functions_package - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
+-- Project Name: Risc V functional CPU 
+-- Description:  the Procedurs for writing into the Outputfile
 -- 
 -- Revision:
 -- Revision 0.01 - File Created
@@ -21,6 +17,8 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use std.textio.all;
+use work.cpu_defs_pack.all;
+use IEEE.numeric_bit.all;       --for to_integer
 
 package output_functions_pack is
 procedure trace(
@@ -36,6 +34,11 @@ procedure trace(
 procedure trace_header(
                 l : inout line;
                 f : inout text);
+                
+procedure mem_dump(
+                l : inout line;
+                f : inout text;
+                mem : in mem_type);
                 
 end package;
 
@@ -62,7 +65,8 @@ procedure trace( l : inout line;
                     write(l, string'(" | "));
                     write(l, integer'image(r2),left, 3);
                     write(l, string'(" | "));
-                    write(l, integer'image(rd), left, 3);                                        
+                    write(l, integer'image(rd), left, 3);    
+                    writeline(f, l);                                    
                 end trace;
                 
 procedure trace_header(
@@ -70,19 +74,51 @@ procedure trace_header(
                 f : inout text) is 
                 begin
                     write(l, string'("PC"), left, 3);
-                    write(l, string'(" | "));
+                    write(l, string'("|"));
                     write(l, string'("OP"), left, 4); --we are calling trace in the different switch cases, so opcode doesnt need to be flexible
-                    write(l, string'(" | "));
+                    write(l, string'("|"));
                     write(l, string'("IMM"), left, 4);
-                    write(l, string'(" | "));                    
+                    write(l, string'("|"));                    
                     write(l, string'("rs1"),left, 3);
-                    write(l, string'(" | "));
+                    write(l, string'("|"));
                     write(l, string'("rs2"),left, 3);
-                    write(l, string'(" | "));
+                    write(l, string'("|"));
                     write(l, string'("rd"), left, 3);
                     writeline(f, l);
                     --divider
-                    write(l, string'("--------------------"));
+                    write(l, string'("---------------------------"),left, 3);
                     writeline(f, l); 
                 end trace_header;
+                
+procedure mem_dump(
+                l : inout line;
+                f : inout text;
+                mem : in mem_type) is 
+                begin
+                    --load last 1000 lines (64535 to 65535) of mem and write them into the Outputfile (f
+                    --Spacing to Trace
+                    write(l, string'(" "));
+                    writeline(f,l);
+                    write(l, string'(" "));
+                    writeline(f,l);
+                    write(l, string'(" "));
+                    writeline(f,l);
+                    write(l, string'("--------Memory DUMP--------"));
+                    writeline(f,l);
+                    --Memory Dump Header
+                    write(l, string'("address"), left, 7);
+                    write(l, string'("|"));
+                    write(l, string'("mem-Content"), left, 11);  --2^31 has 10numbers, so with a "-" we need 11;
+                    writeline(f, l);
+                    write(l, String'("---------------------------"));
+                    writeline(f, l);
+                    --write every address into Outputfile
+                    for i in 64535 to 65535 LOOP
+                        write(l, integer'image(i), left, 7);
+                        write(l, string'("|"));
+                        write(l, integer'image(to_integer(signed(mem(i)))), left, 11);
+                        writeline(f, l);
+                    end LOOP;    
+                end mem_dump;
+                
 end package body;
