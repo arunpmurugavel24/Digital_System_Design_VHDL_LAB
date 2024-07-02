2bd04e3699e304109fce971c4c7d0b3b08674bed
yuhung0112 <168551436+yuhung0112@users.noreply.github.com>
2024-07-02 20:51:27 +0200

    Update data_memory.vhd

--- RISC-V/RTL/VHD/data_memory.vhd
+++ RISC-V/RTL/VHD/data_memory.vhd
@@ -20,34 +20,45 @@ use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.STD_LOGIC_ARITH.ALL;
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
-entity Data_Memory is
+entity DataMemory is
     Port (
         clk        : in  BIT;
         mem_read   : in  BIT;
         mem_write  : in  BIT;
-        addr       : in  BIT_VECTOR(7 downto 0);
+        funct3     : in  BIT_VECTOR(2 downto 0);
+        address    : in  BIT_VECTOR(7 downto 0);
         write_data : in  BIT_VECTOR(7 downto 0);
         read_data  : out BIT_VECTOR(7 downto 0)
     );
-end Data_Memory;
+end DataMemory;
 
-architecture Behavioral of Data_Memory is
+
+architecture Behavioral of DataMemory is
     type memory_type is array (0 to 255) of BIT_VECTOR(7 downto 0); -- 256 x 8-bit memory
     signal memory : memory_type := (others => (others => '0'));
 begin
-    process(clk, mem_write, addr, write_data)
+    process(clk, mem_write, funct3, address, write_data)
     begin
         if clk = '1' and mem_write = '1' then
-            memory(to_integer(unsigned(addr(7 downto 0)))) <= write_data;
+            case funct3 is
+                when "000" => -- SB (Store Byte)
+                    memory(to_integer(unsigned(address))) <= write_data;
+                when others => null;
+            end case;
         end if;
     end process;
 
-    process(clk, mem_read, addr)
+    process(clk, mem_read, funct3, address)
     begin
         if mem_read = '1' then
-            read_data <= memory(to_integer(unsigned(addr(7 downto 0))));
+            case funct3 is
+                when "000" => -- LB (Load Byte)
+                    read_data <= memory(to_integer(unsigned(address)));
+                when others => read_data <= (others => '0');
+            end case;
         else
             read_data <= (others => '0');
         end if;
     end process;
+
 end Behavioral;
