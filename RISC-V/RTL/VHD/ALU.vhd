4267a7f70bd3361fc7764af158adc3599ab7aa1e
peperon321 <hianzing@hotmail.com>
2024-07-03 17:20:45 +0200

    scheiße bit-weise

--- RISC-V/RTL/VHD/ALU.vhd
+++ RISC-V/RTL/VHD/ALU.vhd
@@ -21,6 +21,8 @@
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.numeric_bit.ALL;
+use WORK.conversion_pack.ALL;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
@@ -42,41 +44,56 @@ entity ALU is
 end ALU;
 
 architecture Behavioral of ALU is
-    signal ALU_condition_debug : bit; -- debug
+    signal ALU_condition_result : bit; -- debug
+--    signal a2 : bit_vector(31 downto 0);
+--    signal b2 : bit_vector(31 downto 0);
+    
 begin 
-    process(f, a, b)
+    ALU_condition <= ALU_condition_result;
+    process (f, a, b)
         variable funct3         : bit_vector(2 downto 0);  -- 2:0 of f
         variable ALU_case       : bit_vector(1 downto 0);  -- 4:3 of f
-        variable f2             : bit_vector(4 downto 0) := "00000";  -- debug
-        variable a2             : bit_vector(31 downto 0) := "00000000000000000000000000000000";  -- debug
-        variable b2             : bit_vector (31 downto 0) := "00000000000000000000000000000001";  -- debug
+--        variable f2             : bit_vector(4 downto 0) := "00010";  -- debug
+--        variable a2             : bit_vector(31 downto 0) := "00000000000000000000000000000000";  -- debug
+--        variable b2             : bit_vector (31 downto 0) := "10000000000000000000000000000000";  -- debug
         variable cmp_flag       : bit;  -- 0 or 1 
         variable tmp_result     : bit_vector(31 downto 0);
         variable compare_or     : bit;  -- checks another variable bit by bit, if any bit is 1, then 
+    
+        
     begin
       
---        ALU_case := f(4 downto 3); -- 4:3 of f 
---        funct3 := f(2 downto 0);  -- 2:0 of f
-        ALU_case := f2(4 downto 3);  -- debug
-        funct3 := f2(2 downto 0);  -- debug
+        ALU_case := f(4 downto 3); -- 4:3 of f 
+        funct3 := f(2 downto 0);  -- 2:0 of f
+--        ALU_case := f2(4 downto 3);  -- debug
+--        funct3 := f2(2 downto 0);  -- debug
         compare_or := '0';
+        ALU_condition_result <= '0';
         
         case ALU_case is
         
             -- Cmp --
+            -- Only Branch operations utilise variable 'ALU_condition' 
             when "00" =>
-                report("helo");
-            
+             
                 -- SLT/SLTI --
                 if funct3 = "010" then
-                    report("sup");
+                    if signed(a) < signed(b) then  -- this is synthesisable, and other similar if-cases
+                        outToDMem <= x"00_00_00_01";
+                    else
+                        outToDMem <= x"00_00_00_00";
+                    end if;
                     
                 -- SLTU/SLTIU --
                 elsif funct3 = "011" then
-                    report("sup");
-            
-                -- BEQ/BNE --
-                elsif funct3 = "000" or funct3 = "001" then
+                    if unsigned(a) < unsigned(b) then
+                        outToDMem <= x"00_00_00_01";
+                    else
+                        outToDMem <= x"00_00_00_00";
+                    end if;
+                    
+                -- BEQ --
+                elsif funct3 = "000" then
                     tmp_result := a XOR b;
 --                    tmp_result := a2 XOR b2;  -- debug
                     
@@ -85,50 +102,87 @@ begin
                     end loop;
                         
                         if compare_or = '0' then 
-                            ALU_condition <= '1';  -- BEQ (branch if equal)
---                            ALU_condition_debug <= '1';  -- debug
+                            ALU_condition_result <= '1';  -- BEQ (branch if equal)
+                            outToDMem <= x"00_00_00_01";
+--                            report("HEEEEEELO: compare_or: " & bit'IMAGE(compare_or) & "; ALU_condition: " & bitToString(ALU_condition_result));  -- debug
                         else
-                            ALU_condition <= '0';  -- BNE (branch not equal)
---                            ALU_condition_debug <= '0';  -- debug
+                            ALU_condition_result <= '0';  
+                            outToDMem <= x"00_00_00_00";
+--                            report("HEEEEEELO: compare_or: " & bit'IMAGE(compare_or) & "; ALU_condition: " & bitToString(ALU_condition_result));  -- debug
                         end if;
                         
---                        report("HEEEEEELO: compare_or: " & bit'IMAGE(compare_or) & "; ALU_condition: " & bit'IMAGE(ALU_condition_debug));  -- debug
-                    
+                -- BNE --
+                elsif funct3 = "001" then
+                    tmp_result := a XOR b;
+                    for i in tmp_result'range loop
+                        compare_or := compare_or OR tmp_result(i);
+                    end loop;
+                        
+                        if compare_or = '1' then
+                            ALU_condition_result <= '1';
+                            outToDMem <= x"00_00_00_01";
+                        else 
+                            ALU_condition_result <= '0';
+                            outToDMem <= x"00_00_00_00";
+                        end if;
+                        
                 -- BLT --
                 elsif funct3 = "100" then
+                    if signed(a) < signed (b) then
+                        ALU_condition_result <= '1';
+                        outToDMem <= x"00_00_00_01";
+                    else
+                        ALU_condition_result <= '0';
+                        outToDMem <= x"00_00_00_00";
+                    end if;
                     
                 -- BGE -- 
                 elsif funct3 = "101" then 
-                
+                    if signed(a) >= signed (b) then
+                        ALU_condition_result <= '1';
+                        outToDMem <= x"00_00_00_01";
+                    else 
+                        ALU_condition_result <= '0';
+                        outToDMem <= x"00_00_00_00";
+                    end if;
+                    
                 -- BLTU --
                 elsif funct3 = "110" then
+                    if unsigned(a) < unsigned (b) then
+                        ALU_condition_result <= '1';
+                        outToDMem <= x"00_00_00_01";
+                    else 
+                        ALU_condition_result <= '0';
+                        outToDMem <= x"00_00_00_00";
+                    end if;
                 
                 -- BGEU --
                 elsif funct3 = "111" then
-                
-                -- others --
+                    if signed(a) >= signed (b) then
+                        ALU_condition_result <= '1';
+                        outToDMem <= x"00_00_00_01";
+                    else 
+                        ALU_condition_result <= '0';
+                        outToDMem <= x"00_00_00_00";
+                    end if;
                 end if;
-                
-            
             
             -- Logic -- 
-            when "01" =>
-                report("helo2");
+            when "01" =>                
                 
-                case funct3 is
-                    -- XOR/XORI --
-                    when "100" =>
+                -- XOR/XORI --
+                if funct3 = "100" then
+                    outToDMem <= a XOR b;
                     
-                    -- OR/ORI --
-                    when "110" =>
+                -- OR/ORI --
+                elsif funct3 = "110" then
+                    outToDMem <= a OR b;
+                
+                -- AND/ANDI --
+                elsif funct3 = "111" then
+                    outToDMem <= a AND b;
                     
-                    -- AND/ANDI --
-                    when "111" =>
-                    
-                    -- others --
-                    when others =>
-                    
-                end case;
+                end if;
             
             -- Add --    
             when "10" => 
---
+++ RISC-V/RTL/VHD/auxiliary_package.vhd
@@ -0,0 +1,76 @@
+----------------------------------------------------------------------------------
+-- University: Technical University of Munich
+-- Student: Hian Zing Voon
+-- 
+-- Create Date: 04.06.2024 00:23:53
+-- Design Name: auxiliary_pack
+-- Module Name: auxiliary_pack - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: Contains 2 functions, stringToBitVector & bitVectorToString to be used universally.
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+
+package conversion_pack is  -- Declaration of function
+    
+    -- Conversion of string to bit_vector --
+    function stringToBitVector
+        ( str : string ) return bit_vector;
+    
+    -- Conversion of bit to string --
+    function bitToString
+        ( bit_in : bit ) return string;
+
+end package conversion_pack;
+
+package body conversion_pack is -- Content of function
+    
+    -- Body of conversion of string to bit_vector --
+    function stringToBitVector
+        ( str : string ) return bit_vector is 
+        variable bitVector : bit_vector(str'range);
+    begin
+        -- Loop through char by char in 'str' and change bits in 'bitVector' accordingly --
+        for i in str'range loop
+            if str(i) = '0' then
+                bitVector(i) := '0';
+            elsif str(i) = '1' then
+                bitVector(i) := '1';
+            else
+                report("Error in stringToBitVector: " & str);  -- Report error in Tcl console, if any.
+                exit;
+            end if;
+        end loop;
+        return bitVector;  -- return is a must! Else Vivado will be running in a never ending loop.
+    end function;
+    
+    -- Body of conversion of bit_vector to string --
+    function bitToString
+        ( bit_in : bit) return string is 
+        variable str : string(2 downto 1) := "  ";
+    begin
+        -- Loop through bit by bit in 'bitVector' and change chars in 'str' accordingly --
+            if bit_in = '0' then
+                str(1) := '0';
+            elsif bit_in = '1' then
+                str(1) := '1';
+            else
+                report("Error in bitVectorToString: " & str);  -- Report error in Tcl console, if any.
+            end if;
+        return str;  -- return is a must! Else Vivado will be running in a never ending loop.
+    end function;
+    
+end package body conversion_pack;
+
